6309777601b9bdc8ae832513ccc40cba5e50c2cb
gap <gabrielportela224@gmail.com>
2026-05-20 22:16:59 -0300

    aprendendo a mexer com arquivos em java

---
+++ part4/files/Main.java
@@ -0,0 +1,26 @@
+package part4.files;
+
+import java.util.Scanner;
+import java.nio.file.Paths;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		String input = reader.nextLine();
+		try (Scanner scanner = new Scanner(Paths.get("C:\\Users\\frank\\codigosjava\\MOOCjava\\part4\\files\\" + input))) {
+
+		    // we read the file until all lines have been read
+		    while (scanner.hasNextLine()) {
+		        // we read one line
+		        String row = scanner.nextLine();
+		        // we print the line that we read
+		        System.out.println(row);
+		    }
+		} catch (Exception e) {
+		    System.out.println("Error: " + e.getMessage());
+		}
+	}
+
+}
