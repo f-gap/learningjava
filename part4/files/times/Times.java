5164337c9650c66b8649eb6c78df5bc2659291dd
f-gap <gabrielportela224@gmail.com>
2026-06-12 10:58:48 -0300

    refactor: removendo metodos win() e lose() inuteis e adicionando metodo de registrar resultado do jogo.

--- part4/files/times/Times.java
+++ part4/files/times/Times.java
@@ -2,36 +2,38 @@ package part4.files.times;
 
 public class Times {
 	private String nome;
-	private int pontos;
 	private int jogos;
 	private int vitorias;
 	private int derrotas;
 	private int empates;
 	
-	Times (String nome, int pontos) {
+	Times (String nome) {
 		this.nome = nome;
-		this.pontos = pontos;
 	}
 	
 	@Override
 	public String toString () {
-		return "Time: " + nome + ", partidas jogadas: " + jogos; 
+		return "Time: " + nome + ", partidas jogadas: " + jogos + " vitorias: " + vitorias + " derrotas: " + derrotas;
 	}
-	
+
 	public String getNome() {return nome;}
-	public int getPontos() {return pontos;}
 	public int getJogos() {return jogos;}
-	
+
 	public void addJogo() {this.jogos++;}
 	public void addVitoria() {this.vitorias++;}
 	public void addDerrota() {this.derrotas++;}
 	public void addEmpate() {this.empates++;}
-	
-	public void win() {
-		vitorias++;
-	}
-	
-	public void lose() {
-		derrotas++;
+
+	public void registrarResultados(int pontosTimeDaCasa,int pontosAdversario) {
+		addJogo();
+		if(pontosTimeDaCasa > pontosAdversario) {
+			this.addVitoria();
+		}
+		else if(pontosTimeDaCasa < pontosAdversario) {
+			this.addDerrota();
+		}
+		else {
+			this.addEmpate();
+		}
 	}
 }
